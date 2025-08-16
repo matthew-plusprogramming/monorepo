fca1d49da03bec2090c87478fc4619f305a050f5
Matthew Lin <matthewlinplusprogramming@gmail.com>
2025-08-16 13:42:44 -0700

    chore: Require yield eslint

--- apps/node-server/eslint.config.ts
+++ apps/node-server/eslint.config.ts
@@ -1,10 +1,3 @@
 import { baseConfig } from '@configs/eslint-config';
 
-export default [
-  ...baseConfig(__dirname, ['tsconfig.lint.json']),
-  {
-    rules: {
-      'require-yield': 'warn',
-    },
-  },
-];
+export default [...baseConfig(__dirname, ['tsconfig.lint.json'])];
