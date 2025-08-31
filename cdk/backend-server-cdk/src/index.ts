3d03230126f8e4733cdcdd59a4b4577d52bc5ea3
Matthew Lin <matthewlinplusprogramming@gmail.com>
2025-08-31 13:17:16 -0700

    fix: Process only one stack at once to prevent circular dependencies

--- cdk/backend-server-cdk/src/index.ts
+++ cdk/backend-server-cdk/src/index.ts
@@ -5,6 +5,7 @@ import { App } from 'cdktf';
 import { stacks } from './stacks';
 
 const region = process.env.AWS_REGION;
+const selectedStack = process.env.STACK;
 
 if (!region) {
   throw new Error('AWS_REGION environment variable is not set');
@@ -13,6 +14,11 @@ if (!region) {
 const app = new App();
 
 stacks.forEach((stack) => {
+  // Only one stack to be processed at a time
+  if (selectedStack && selectedStack !== stack.name) {
+    return;
+  }
+
   const { Stack } = stack;
   new Stack(app, stack.name, {
     region,
