7f1a3aabb03efcb30ffca298103e08609190ec1d
Matthew Lin <matthewlinplusprogramming@gmail.com>
2025-09-01 11:01:16 -0700

    chore: Remove todo comment

--- apps/client-website/vite.config.ts
+++ apps/client-website/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { baseConfig } from '@configs/vite-config';
 
-// TODO: MAKE THE CONFIGS BUILD AS WELL SO THEY CAN BE IMPORTED CROSS PROJECT
 export default defineConfig({
   ...baseConfig,
   plugins: [react()],
