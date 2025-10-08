79829335a1aac2262584834d181ddd4924d3cd79
Matthew Lin <matthewlinplusprogramming@gmail.com>
2025-10-08 16:40:49 -0700

    refactor: Make heartbeat handler async

--- apps/node-server/src/handlers/heartbeat.handler.ts
+++ apps/node-server/src/handlers/heartbeat.handler.ts
@@ -1,6 +1,6 @@
 import { HTTP_RESPONSE } from '@packages/backend-core';
 import type { RequestHandler } from 'express';
 
-export const heartbeatRequestHandler: RequestHandler = (_req, res) => {
+export const heartbeatRequestHandler: RequestHandler = async (_req, res) => {
   return res.status(HTTP_RESPONSE.SUCCESS).send('OK');
 };
