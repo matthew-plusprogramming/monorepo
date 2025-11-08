dc42b1cf8c718104a40c75f58fbfae823115e48e
Matthew Lin <matthewlinplusprogramming@gmail.com>
2025-11-08 01:34:56 -0800

    chore: Switch to react config

--- apps/client-website/eslint.config.ts
+++ apps/client-website/eslint.config.ts
@@ -4,14 +4,14 @@ import nextTs from 'eslint-config-next/typescript';
 
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
-import { baseConfig } from '@configs/eslint-config';
+import { reactConfig } from '@configs/eslint-config';
 
 import { fileURLToPath } from 'url';
 import path from 'path';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default [
-  ...baseConfig(__dirname, ['tsconfig.json']),
+  ...reactConfig(__dirname, ['tsconfig.json']),
   ...nextVitals,
   ...nextTs,
   {
