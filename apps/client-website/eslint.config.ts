f4fc87ca9067831518a4d032d5fd0c104187936d
Matthew Lin <matthewlinplusprogramming@gmail.com>
2025-11-08 01:36:23 -0800

    fix: Remove react config

--- apps/client-website/eslint.config.ts
+++ apps/client-website/eslint.config.ts
@@ -4,14 +4,14 @@ import nextTs from 'eslint-config-next/typescript';
 
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
-import { reactConfig } from '@configs/eslint-config';
+import { baseConfig } from '@configs/eslint-config';
 
 import { fileURLToPath } from 'url';
 import path from 'path';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default [
-  ...reactConfig(__dirname, ['tsconfig.json']),
+  ...baseConfig(__dirname, ['tsconfig.json']),
   ...nextVitals,
   ...nextTs,
   {
