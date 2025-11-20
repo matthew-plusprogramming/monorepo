73d130a0aec5ee13f1cfc2c6a19fe7894f96dcc1
Matthew Lin <matthewlinplusprogramming@gmail.com>
2025-11-19 16:12:30 -0800

    feat: Implement new http errors

--- packages/core/backend-core/src/types/errors/http.ts
+++ packages/core/backend-core/src/types/errors/http.ts
@@ -3,12 +3,28 @@ import { Data } from 'effect';
 import type { GenericErrorPayload } from '@/types/errors/index.js';
 
 // 400 level errors
+export class BadRequestError extends Data.TaggedError(
+  'BadRequestError',
+)<GenericErrorPayload> {}
+
+export class UnauthorizedError extends Data.TaggedError(
+  'UnauthorizedError',
+)<GenericErrorPayload> {}
+
+export class ForbiddenError extends Data.TaggedError(
+  'ForbiddenError',
+)<GenericErrorPayload> {}
+
 export class NotFoundError extends Data.TaggedError(
   'NotFoundError',
 )<GenericErrorPayload> {}
 
 export class ConflictError extends Data.TaggedError(
   'ConflictError',
+)<GenericErrorPayload> {}
+
+export class ThrottledError extends Data.TaggedError(
+  'ThrottledError',
 )<GenericErrorPayload> {}
 
 // 500 level errors
@@ -18,4 +34,8 @@ export class InternalServerError extends Data.TaggedError(
 
 export class BadGatewayError extends Data.TaggedError(
   'BadGatewayError',
+)<GenericErrorPayload> {}
+
+export class ServiceUnavailableError extends Data.TaggedError(
+  'ServiceUnavailableError',
 )<GenericErrorPayload> {}
