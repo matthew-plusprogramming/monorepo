c56cce32bb7820e359999b2d32fa936741cf093e
Matthew Lin <matthewlinplusprogramming@gmail.com>
2025-12-26 20:51:52 -0800

    chore: export dropdown menu styles for real

--- packages/core/ui-components/src/components/DropdownMenu/index.tsx
+++ packages/core/ui-components/src/components/DropdownMenu/index.tsx
@@ -1,2 +1,2 @@
 export type { DropdownMenuItem, DropdownMenuProps } from './DropdownMenu';
-export { DropdownMenu } from './DropdownMenu';
+export { DropdownMenu, DropdownMenuStyles } from './DropdownMenu';
